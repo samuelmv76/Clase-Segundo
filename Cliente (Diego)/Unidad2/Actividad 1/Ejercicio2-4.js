bf8d5520243d102c9b327281370ebf257c6572e3
Samuel Martos (Casa) <samuelmv76@educastur.es>
2024-10-04 20:59:51 +0200

    Ejercicio 2-4 terminado

--- Cliente (Diego)/Unidad2/Actividad 1/Ejercicio2-4.js
+++ Cliente (Diego)/Unidad2/Actividad 1/Ejercicio2-4.js
@@ -1,6 +1,19 @@
 function tiempoDeVida() {
-    let num1= prompt("Introduce tu nombre: ");
-    let num2= prompt("Introduce tu edad: ");
-
-    num2*365
+    try {
+        let nombre= prompt("Introduce tu nombre: ");
+        let num= prompt("Introduce tu edad: ");
+        parseInt(num);
+        if (num2 == 0) {
+            throw new Error("Error: No se puede dividir por 0.");
+        } 
+        if((num1.trim() || num2.trim()) == "") {
+            throw new Error ("empty");
+        }
+        tiempo=num*365;
+        //Haz un programa que nos pregunte nuestro nombre, nuestra edad. El programa nos tiene que dar 
+        //como resultado los días que hemos vivido hasta el momento (suponiendo años de 365 días).
+        document.write(tiempo);
+    } catch (error) {
+        alert(error.message)
+    }
 }
---
+++ Cliente (Diego)/Unidad2/Actividad 1/Ejercicio2-5.js
@@ -0,0 +1,4 @@
+/*
+Haz un programa que nos pida un número, y nos muestre en una única pantalla, el doble, el triple 
+y el cuádruple del número que habíamos introducido (cada uno en una línea).
+*/

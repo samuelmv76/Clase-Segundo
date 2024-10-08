a5f406e7a29917b82139c3f7a235870439c513fd
Samuel Martos (clase) <samuelmv76@educastur.es>
2024-10-08 10:38:44 +0200

    Ejercicio2-8 terminado

---
+++ Cliente (Diego)/Unidad2/Actividad 1/Ejercicio2-8.js
@@ -0,0 +1,21 @@
+/*
+Haz un programa que funcione de la manera siguiente:
+• Nos pida nuestro nombre.
+• Nos pida nuestro primer apellido.
+• Nos pida en que población vivimos.
+• Al final nos presente:
+=======================
+Hola “nombre y apellido”
+Adiós habitante de “Población”
+======================
+*/
+function name() {
+    let nombre=prompt("Introduce tu nombre: ");
+    let apellido=prompt("Introduce tu primer apellido: ");
+    let poblacion=prompt("Introduce la poblacion: ");
+
+    document.write("=======================</br>")
+    document.write("Hola ",nombre," ",apellido,"</br>")
+    document.write("Adiós habitante de ",poblacion,"</br>")
+    document.write("=======================")
+}

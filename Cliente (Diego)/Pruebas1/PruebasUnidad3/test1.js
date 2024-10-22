051b49a822f7e8365ab13b5c2544faabf07ecb61
Samuel Clase <samuelmv04@gmail.com>
2024-10-22 11:49:37 +0200

    Ejercicios js

---
+++ Cliente (Diego)/Pruebas1/PruebasUnidad3/test1.js
@@ -0,0 +1,13 @@
+function esPar(n) {
+    return n%2==0;
+}
+function testUnidad3(){
+    let numeros = [1,2,2,33,123];
+    let nuemerosPares = numeros.filter(esParLambda);
+    document.write(encontradorsVerde.join(', '));
+
+    let colores = ['rojo','verde','rojo'];
+}
+
+
+//
---
+++ Cliente (Diego)/Unidad3/Ejercicio1.js
@@ -0,0 +1,16 @@
+function vernumeros() {
+/*
+    Hacer un programa que contenga una 
+    función llamada vernumeros() 
+    que permita visualizar los 10
+    primeros números tantas veces como queramos
+*/
+let final = prompt('Cuantas vezes quieres ver los 10 primeros numeros?');
+parseInt(final);
+for (let i=1; i<=final; i++){
+    for (let i = 0; i <= 10; i++) {
+            document.write(i," ");
+    }
+    document.write("<br>");
+}
+}

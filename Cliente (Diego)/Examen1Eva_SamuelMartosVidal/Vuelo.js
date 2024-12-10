0cae56a5727aeb7099959bfbc9d16b24bc5110c3
Samuel Martos Clase <samuelmv04@gmail.com>
2024-12-10 10:26:08 +0100

    modificar terminado

--- Cliente (Diego)/Examen1Eva_SamuelMartosVidal/Vuelo.js
+++ Cliente (Diego)/Examen1Eva_SamuelMartosVidal/Vuelo.js
@@ -5,8 +5,8 @@ class Vuelo {
         this.importe = parseFloat(importe);
     }
     modificarVuelo(nuevoPlazas, nuevaImporte) {
-        this.numPlazas = nuevoPlazas;
-        this.importe = nuevaImporte;
+        this.numPlazas = parseFloat(nuevoPlazas);
+        this.importe = parseFloat(nuevaImporte);
     }
 }
 // Variables globales
@@ -19,31 +19,26 @@ function anadirPersona() {
     if (codigo && numPlazas && importe) {
         const nuevoVuelo = new Vuelo(codigo, numPlazas, importe);
         vuelos.push(nuevoVuelo);
-        console.log(nuevoVuelo);
-        console.log(vuelos);
         actualizarTabla();
         limpiarCampos();
     } else {
         alert('Completa todos los campos.');
     }
-    console.log(codigo);
-}//terminado
+}
 function modificarPersona() {
     const codigo = document.getElementById('codigo').value.trim();
-    const numPlazas = document.getElementById('numPlazas').value;
-    const importe = document.getElementById('importe').value;
-    console.log(codigo);
-    
+    const numPlazas = parseFloat(document.getElementById('numPlazas').value);
+    const importe = parseFloat(document.getElementById('importe').value);
     const vuelo = vuelos.find(v => v.codigo === codigo);
     if (vuelo) {
-        vuelo.modificarPersona(numPlazas, importe);
+        vuelo.modificarVuelo(numPlazas, importe);
         actualizarTabla();
         limpiarCampos();
     } else {
-        alert('El vuelo no esta registrada.');
+        alert('El vuelo no está registrado.');
     }
-}//terminado
-function actualizarTabla() { //usada en modificar y anadir
+}
+function actualizarTabla() {
     const tabla = document.getElementById('tablaVuelos').querySelector('tbody');
     tabla.innerHTML = '';
     vuelos.forEach(vuelo => {
@@ -52,15 +47,13 @@ function actualizarTabla() { //usada en modificar y anadir
                 <td>${vuelo.codigo}</td>
                 <td>${vuelo.numPlazas}</td>
                 <td>${vuelo.importe}</td>
-                <td>-</td>
-                <td>-</td>
             </tr>
         `;
         tabla.innerHTML += fila;
     });
-}//terminado
+}
 function limpiarCampos() {
     document.getElementById('codigo').value = '';
     document.getElementById('numPlazas').value = '';
     document.getElementById('importe').value = '';
-}//terminado
+}

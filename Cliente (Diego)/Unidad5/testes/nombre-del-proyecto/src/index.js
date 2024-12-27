f90f87aa4a2dbc05b83d4d612dae3562f85bf37a
Samuel Martos (Casa) <samuelmv04@gmail.com>
2024-12-27 02:07:37 +0100

    tuto no hay chance

--- Cliente (Diego)/Unidad5/testes/nombre-del-proyecto/src/index.js
+++ Cliente (Diego)/Unidad5/testes/nombre-del-proyecto/src/index.js
@@ -16,11 +16,29 @@ function insertarTabla() {
             </td>
         </th>
     </table>;
-    const insertfila=ReactDOM.createRoot(rootElement);
+    const tablaElement = document.getElementById('tabla');
+    const insertfila=ReactDOM.createRoot(tablaElement);
     insertfila.render(filaElement);
 };
 
-document.addEventListener("click", function() {
-    document.getElementById("bot").click();
-    insertarTabla();   
+
+
+const botonin=document.getElementById("bot");
+botonin.addEventListener("click", function() {
+    //COMPROBAR SI EXISTE
+    insertarTabla();
+    //borrar el boton
+    botonin.remove();
 });
+
+//MIDU
+const appDomElement = document.getElementById('app');
+
+const rootApp = ReactDOM.createRoot(appDomElement);
+const button = React.createElement('button',{"data-id":123}, 'Boton 1');
+const button2 = React.createElement('button',{"data-id":456}, 'Boton 2');
+const button3 = React.createElement('button',{"data-id":789}, 'Boton 3');
+const div = React.createElement('div', null, [button,button2,button3]);
+rootApp.render(div);
+
+//MIDU FIN

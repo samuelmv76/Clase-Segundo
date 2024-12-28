2ddb93c7516474b6e756d6ce5ed638e846f90ed3
Samuel Martos (Casa) <samuelmv04@gmail.com>
2024-12-28 02:59:22 +0100

    ejemplo de componente

--- Cliente (Diego)/Unidad5/testes/react-vite-1/src/main.jsx
+++ Cliente (Diego)/Unidad5/testes/react-vite-1/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import App from './App.jsx'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const Boton = ({text}) => {
+  return (
+    <button>
+    
+      {text}
+    </button>
+  )
+}
+root.render(
+  <React.Fragment>
+    <Boton text="Button 1"/>
+    <Boton text="Button 2"/>
+  </React.Fragment>
 )
